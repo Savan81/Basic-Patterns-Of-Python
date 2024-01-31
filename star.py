77acc9d41cb8551f6953af25abe2a1d2a7d7b6c2
Savan <117834137+Savanji@users.noreply.github.com>
2024-01-31 12:45:01 +0530

    Add files via upload

---
+++ star.py
@@ -0,0 +1,17 @@
+# * 
+
+# * *
+
+# * * *
+
+# * * * *
+
+# * * * * *
+
+for i in range(1,6): # Outer (Row's) - Start's from 1 and end's at 5
+
+    for j in range(1,i+1): # Inner (Colum's)
+
+        print("*", end=' ') # End For End Character
+        
+    print("\n") # New Line 
